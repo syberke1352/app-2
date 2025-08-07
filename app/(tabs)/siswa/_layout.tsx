d1b7a1d82f6747fae6b6b55079c61389496a78f8
syberke1352 <jaisyurrohmanb@gmail.com>
2025-08-07 22:10:20 +0700

    Updated _layout.tsx

--- app/(tabs)/siswa/_layout.tsx
+++ app/(tabs)/siswa/_layout.tsx
@@ -1,5 +1,5 @@
 import { Tabs } from 'expo-router';
-import { BookOpen, CloudUpload, Home, HousePlus, ListChecks, Trophy, User } from 'lucide-react-native';
+import { BookOpen, CloudUpload, Chrome as Home, HousePlus, ListChecks, Trophy, User } from 'lucide-react-native';
 import { Platform } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
